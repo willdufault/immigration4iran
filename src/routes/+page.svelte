b9e1fbeed675d146718c32cada0b1edb1a1089f6
wduf <wduf02@gmail.com>
2023-03-15 13:23:25 +0100

    working on header

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<link rel="icon" href="./favicon.png" type="image/png" />
+	<title>Immigration4Iran</title>
+</svelte:head>
+<main>
+	<Header/>
+</main>
+<style>
+
+</style>
+<script>
+	import { base } from "$app/paths";
+    import Header from "./header.svelte";
+</script>
---
+++ src/routes/header.svelte
@@ -0,0 +1,52 @@
+<main>
+	<div class={"header-wrapper"}>
+		<div class={"logo-wrapper"}>
+			<p>Immigration4Iran</p>
+		</div>
+		<div class={"nav-wrapper"}>
+			<div class={"nav-item"}>
+				<p>Legal <Icon icon="ph:caret-down" /></p>
+			</div>
+			<div class={"nav-item"}>
+				<p>Social Services <Icon icon="ph:caret-down" /></p>
+			</div>
+			<div class={"nav-item"}>
+				<p>Community Building <Icon icon="ph:caret-down" /></p>
+			</div>
+			<div class={"nav-item"}>
+				<p>Language <Icon icon="ph:caret-down" /></p>
+			</div>
+		</div>
+	</div>
+</main>
+<style>
+	*
+	{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	.header-wrapper
+	{
+		border: 3px solid blue;
+		padding: 2vmin;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.nav-wrapper
+	{
+		display: flex;
+		gap: 5%;
+	}
+
+	.nav-item
+	{
+		display: flex;
+	}
+</style>
+<script>
+	import Icon from '@iconify/svelte';
+</script>
