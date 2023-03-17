c7e4bca47b712eaa9c2d6293b2127e13cbe66292
adelinemae <98119483+adelinemae@users.noreply.github.com>
2023-03-17 11:27:00 +0100

    added dictionary class and testing example
    created dictionary class for having different languages for different text blocks, and provided a code example of how to use it

---
+++ src/routes/dictionary.js
@@ -0,0 +1,37 @@
+const Languages = {
+    English: Symbol("English"),
+    German: Symbol("German"),
+    Persian: Symbol("Persian"),
+};
+export class Dictionary {
+    dict;
+    languages = Languages;
+    constructor() {
+        this.dict = new Map();
+        for (let lang in Languages) {
+            this.dict.set(lang, new Map());
+        }
+    }
+
+    /**
+     * this will add a translation for a given text block
+     * @param lang the langauge of the text
+     * @param text the text being added
+     */
+    setText(lang, text) {
+        this.dict.set(lang, text)
+    }
+
+    getText(lang) {
+        return this.dict.get(lang);
+    }
+
+    /**
+     * This function returns the enums to be used across the website, standardizes code for different languages
+     * @returns {{English: symbol, Persian: symbol, German: symbol}}
+     * the current languages that translations are available for
+     */
+    static get languages() {
+    }
+
+}
