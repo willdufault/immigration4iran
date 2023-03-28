4064aa58aefb930514c03a7a54b27b40283b8442
wduf <wduf02@gmail.com>
2023-03-28 13:38:17 +0200

    working on global styling

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,76 +1,76 @@
 <main>
-	<div class={"header-wrapper"}>
-		<div class={"logo-wrapper"}>
+	<div class="header-wrapper">
+		<div class="logo-wrapper">
 			<p>{title.getText()}</p>
 		</div>
-		<div class={"nav-wrapper"}>
-			<div class={"nav-item"} on:mouseover={() => legal_down = true} on:focus={{}} on:mouseleave={() => legal_down = false}>
-				<div class={"nav-title"}>
+		<div class="nav-wrapper">
+			<div class="nav-item" on:mouseover={() => legal_down = true} on:focus={{}} on:mouseleave={() => legal_down = false}>
+				<div class="nav-title">
 					<p>{legal.getText()}</p> <Icon icon="ph:caret-down"/>
 				</div>
 				{#if legal_down}
-					<div class={"dropdown-wrapper"}>
-						<div class={"dropdown-item"}>
+					<div class="dropdown-wrapper">
+						<div class="dropdown-item">
 							<p>thing 1</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 2</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 3</p>
 						</div>
 					</div>
 				{/if}
 			</div>
-			<div class={"nav-item"} on:mouseover={() => social_down = true} on:focus={{}} on:mouseleave={() => social_down = false}>
-				<div class={"nav-title"}>
+			<div class="nav-item" on:mouseover={() => social_down = true} on:focus={{}} on:mouseleave={() => social_down = false}>
+				<div class="nav-title">
 					<p>Social Services</p>&nbsp;<Icon icon="ph:caret-down"/>
 				</div>
 				{#if social_down}
-					<div class={"dropdown-wrapper"}>
-						<div class={"dropdown-item"}>
+					<div class="dropdown-wrapper">
+						<div class="dropdown-item">
 							<p>thing 1</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 2</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 3</p>
 						</div>
 					</div>
 				{/if}
 			</div>
-			<div class={"nav-item"} on:mouseover={() => comm_down = true} on:focus={{}} on:mouseleave={() => comm_down = false}>
-				<div class={"nav-title"}>
+			<div class="nav-item" on:mouseover={() => comm_down = true} on:focus={{}} on:mouseleave={() => comm_down = false}>
+				<div class="nav-title">
 					<p>Community Building</p>&nbsp;<Icon icon="ph:caret-down"/>
 				</div>
 				{#if comm_down}
-					<div class={"dropdown-wrapper"}>
-						<div class={"dropdown-item"}>
+					<div class="dropdown-wrapper">
+						<div class="dropdown-item">
 							<p>thing 1</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 2</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 3</p>
 						</div>
 					</div>
 				{/if}
 			</div>
-			<div class={"nav-item"} on:mouseover={() => lang_down = true} on:focus={{}} on:mouseleave={() => lang_down = false}>
-				<div class={"nav-title"}>
+			<div class="nav-item" on:mouseover={() => lang_down = true} on:focus={{}} on:mouseleave={() => lang_down = false}>
+				<div class="nav-title">
 					<p>Language</p>&nbsp;<Icon icon="ph:caret-down"/>
 				</div>
 				{#if lang_down}
-					<div class={"dropdown-wrapper"} style={"right: 0; left: auto; text-align: right;"}>
-						<div class={"dropdown-item"}>
+					<div class="dropdown-wrapper">  <!-- style="right: 0; left: auto;" -->
+						<div class="dropdown-item">
 							<p>thing 1</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 2</p>
 						</div>
-						<div class={"dropdown-item"}>
+						<div class="dropdown-item">
 							<p>thing 3</p>
 						</div>
 					</div>
@@ -113,21 +113,23 @@
 		display: flex;
 		align-items: center;
 		height: 8vmin;
+		width: 15vw;
 		padding: 0 2vmin;
 		border: 1px solid purple;
+		justify-content: space-between;
 	}
 
 	.dropdown-wrapper
 	{
 		position: absolute;
-		padding: 2vmin;
 		min-width: 15vw;
 		border: 2px solid green;
 	}
 
 	.dropdown-item
 	{
-		
+		padding: 1vmin;
+		border: 3px double red;
 	}
 
 	.nav-title:hover
