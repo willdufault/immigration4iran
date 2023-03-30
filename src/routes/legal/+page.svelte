0b89a8055acd8b17d61da0441db22889467aa089
venvz <99231469+venvz@users.noreply.github.com>
2023-03-30 14:17:49 +0200

    Update +page.svelte

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,6 +1,20 @@
 <main>
-    <Header/>
-    <p> {component.title.getText()}</p>
+    <PageWrapper>
+        <Header/>
+        <p> {component.title.getText()}</p>
+
+        <div class="flip">
+            <div class="card-front">
+                <h1>Hello</h1>/
+            </div>
+            <div class="card-back">
+                <h1>Yeppers</h1>
+            </div>
+        </div>
+        
+
+
+    </PageWrapper>
 </main>
 
 <style>
@@ -10,6 +24,23 @@
         padding: 0;
         box-sizing: border-box;
     }
+    .flip 
+    {
+        background-color: transparent;
+        width: 300px;
+        height: 200px;
+        border: 1px solid #f1f1f1;
+    }
+    .card-front, .card-back
+    {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+    }
+
+
 </style>
 
 <script>
@@ -18,4 +49,5 @@
     import TextBlock from "../textBlock.svelte";
     import {Dictionary} from "../dictionary.js";
     import * as component from "./legalText.js";
+    import PageWrapper from "../pageWrapper.svelte";
 </script>
