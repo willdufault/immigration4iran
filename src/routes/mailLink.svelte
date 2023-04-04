d42d64b92209eb2ae0e46c010b91f1049afc14c4
adelinemae <98119483+adelinemae@users.noreply.github.com>
2023-04-04 12:04:10 +0200

    mailLink component
    created custom component to handle mailing links on ngo page

---
+++ src/routes/mailLink.svelte
@@ -0,0 +1,8 @@
+<main>
+    <a href="mailto:{ngoContact}" target="_blank" rel="noreferrer"> {baseContact} {ngoContact} </a>
+</main>
+
+<script>
+    export let baseContact;
+    export let ngoContact;
+</script>
