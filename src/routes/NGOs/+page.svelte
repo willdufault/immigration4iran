a844b74fbccf7ac01a4325628329958ccbe8d5a5
adelinemae <98119483+adelinemae@users.noreply.github.com>
2023-04-17 15:09:00 +0200

    search term update and deleted old stuff

--- src/routes/NGOs/+page.svelte
+++ src/routes/NGOs/+page.svelte
@@ -7,7 +7,7 @@
 				<div class="filter-list">
 					<form>
 						<label for="searchbar">Search:</label>
-						<input type="search" id="searchbar">
+						<input type="search" id="searchbar" bind:value={search}>
 						<br>
 						<input type="checkbox" id="legal">
 						<label for="legal">Legal</label>
@@ -22,91 +22,21 @@
 			</div>
 			<div class="ngo-wrapper">
 				<button bind:this={toggle_menu_button} class="toggle-filter-menu-button" on:click={() => toggleFilterMenu()}>&lt;</button>
-				<ul>
-					{#each ngoArray as ngo}
-						{#if ngo.getString().indexOf("")!==-1}
-							<TextBlock>
-<!--								<MyLink slot="title" link={ngo.getLink()} text={ngo.getTitle()}/>-->
-								<a slot="title" href={ngo.getLink()} target="_blank" rel="noreferrer"> {ngo.getTitle()} </a>
-								<p slot="languages"> {ngo.getGenLangs()} {ngo.getLangs()} </p>
-								<p slot="body"> {ngo.getBody()} </p>
-								<MailLink slot="contact" ngoContact= {ngo.getContact()} ngoDisplay={ngo.getContactDisplay()}/>
-							</TextBlock>
-						{/if}
-					{/each}
-				</ul>
 				<p class="ngo-title"> {component.title.getText()}</p>
 				<div class="block-wrapper">
-					{#if true}
-						<p>this is showing</p>
-					{/if}
-					<TextBlock>
-						<!-- <Icon slot="icon" icon="carbon:face-cool" /> -->
-						<a slot="title" href="https://www.welcome-united.org" target="_blank" rel="noreferrer"> {component.wu_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.wu_lang.getText()} </p>
-						<p slot="body"> {component.wu_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.wu_contact.getText()} ngoDisplay={component.wu_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="http://jogspace.net" target="_blank" rel="noreferrer"> {component.jog_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.jog_lang.getText()} </p>
-						<p slot="body"> {component.jog_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.jog_contact.getText()} ngoDisplay={component.jog_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="http://thecaravan.org" target="_blank" rel="noreferrer"> {component.kar_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.kar_lang.getText()} </p>
-						<p slot="body"> {component.kar_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.kar_contact.getText()} ngoDisplay={component.kar_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="http://www.thevoiceforum.org/" target="_blank" rel="noreferrer"> {component.voice_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.voice_lang.getText()} </p>
-						<p slot="body"> {component.voice_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.voice_contact.getText()} ngoDisplay={component.voice_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://www.proasyl.de" target="_blank" rel="noreferrer"> {component.pa_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.pa_lang.getText()} </p>
-						<p slot="body"> {component.pa_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.pa_contact.getText()} ngoDisplay="proasyl@proasyl.de"/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://w2eu.info" target="_blank" rel="noreferrer"> {component.weu_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.weu_lang.getText()} </p>
-						<p slot="body"> {component.weu_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.weu_contact.getText()} ngoDisplay={component.weu_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://www.frauenrechte.de/" target="_blank" rel="noreferrer"> {component.tdf_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.tdf_lang.getText()} </p>
-						<p slot="body"> {component.tdf_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.tdf_contact.getText()} ngoDisplay={component.tdf_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://www.medibuero-kiel.de/" target="_blank" rel="noreferrer"> {component.mk_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.mk_lang.getText()} </p>
-						<p slot="body"> {component.mk_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.mk_contact.getText()} ngoDisplay="info@medibuero-kiel.de"/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://verband-dsh.de/ussa/" target="_blank" rel="noreferrer"> {component.ussa_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.ussa_lang.getText()} </p>
-						<p slot="body"> {component.ussa_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.ussa_contact.getText()} ngoDisplay={component.ussa_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://www.unitedforukraine.org" target="_blank" rel="noreferrer"> {component.ufu_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.ufu_lang.getText()} </p>
-						<p slot="body"> {component.ufu_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.ufu_contact.getText()} ngoDisplay={component.ufu_contact.getText()}/>
-					</TextBlock>
-					<TextBlock>
-						<a slot="title" href="https://immigration4ukraine.eu/" target="_blank" rel="noreferrer"> {component.ifu_title.getText()} </a>
-						<p slot="languages"> {component.available_langs.getText()} {component.ifu_lang.getText()} </p>
-						<p slot="body"> {component.ifu_body.getText()} </p>
-						<MailLink slot="contact" ngoContact= {component.ifu_contact.getText()} ngoDisplay={component.ifu_contact.getText()}/>
-					</TextBlock>
+					<ul>
+						{#each ngoArray as ngo}
+							{#if ngo.getString().indexOf(search)!==-1}
+								<TextBlock>
+	<!--								<MyLink slot="title" link={ngo.getLink()} text={ngo.getTitle()}/>-->
+									<a slot="title" href={ngo.getLink()} target="_blank" rel="noreferrer"> {ngo.getTitle()} </a>
+									<p slot="languages"> {ngo.getGenLangs()} {ngo.getLangs()} </p>
+									<p slot="body"> {ngo.getBody()} </p>
+									<MailLink slot="contact" ngoContact= {ngo.getContact()} ngoDisplay={ngo.getContactDisplay()}/>
+								</TextBlock>
+							{/if}
+						{/each}
+					</ul>
 				</div>
 			</div>
 		</div>
@@ -192,6 +122,7 @@
 	import {onMount} from "svelte";
 
 	let ngoArray = [];
+	let search = "";
 
 	loadNGOArray();
 
