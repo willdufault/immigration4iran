134ecf98f99964a295bd84715c1f0bf678a8c68d
venvz <99231469+venvz@users.noreply.github.com>
2023-04-17 14:18:08 +0200

    Updated Home Page (Not Working)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,26 @@
 <main>
 	<PageWrapper>
 		<Header/>
-		<div class="sdg-wrapper">
-			<img on:click={() => window.open(`${base}/legal`)} src="https://conflictoflaws.net/News/2019/04/english_SDG_17goals_poster_all_languages_with_UN_emblem_1.png" alt="sdgs"/>
+
+        <div class="title-wrapper">
+			<p>{component.intro.getText()}something</p>
+			<p>|</p>
+			<p>{component.introText.getText()}</p>
 		</div>
-		<p class="roadmap-title">Legal Roadmap</p>
+		<div class="tiles-container">
+			<div class="tile center-text" style="background:#348888;">
+				<p>a</p>
+				<div class="tile-cover center-text" style="background:#DAFDBA">
+					<p>b</p>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- <div class="sdg-wrapper">
+			<img on:click={() => window.open(`${base}/legal`)} src="https://conflictoflaws.net/News/2019/04/english_SDG_17goals_poster_all_languages_with_UN_emblem_1.png" alt="sdgs"/>
+		</div> -->
+		<p class="roadmap-title">About Us</p>
 	</PageWrapper>
 </main>
 <style>
@@ -51,11 +67,67 @@
 		font-size: 3rem;
 		font-family: var(--font1);
 	}
+	.title-wrapper
+	{
+		margin: 0 20%; 
+		display: flex; 
+		justify-content: space-around;
+		font-size: 2.5rem;
+		font-weight: 500;
+		margin: 4% 10%;
+	}
+
+	.tiles-container
+	{
+		margin: 0 10%;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 2%;
+		row-gap: 2vh;
+		padding-bottom: 5%;
+	}
+
+	.center-text
+	{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
+
+	.tile
+	{
+		/* font-size: 1.1rem; */
+		position: relative;
+		padding: 5%;
+		min-height: 30vh;
+		width: 30%;
+		display: flex;
+		flex-grow: 1;
+		border-radius: 0.2rem;
+	}
+
+	.tile-cover
+	{
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		border-radius: inherit;
+		padding: inherit;
+	}
+
+	.tile:hover .tile-cover
+	{
+		display: none;
+	}
 </style>
 <script>
 	import { base } from "$app/paths";
     import Header from "./header.svelte";
 	import TextBlock from "./textBlock.svelte";
+	import * as component from "./mainPage.js";
 	import {Dictionary} from "./dictionary.js";
 	import PageWrapper from "./pageWrapper.svelte";
 </script>
---
+++ src/routes/mainPage.js
@@ -0,0 +1,89 @@
+import { Dictionary } from "./dictionary.js";
+
+let langs = Dictionary.languages;
+
+let english = langs.English;
+let german = langs.German;
+let persian = langs.Persian;
+
+export let available_langs = new Dictionary();
+available_langs.setText(english, "Available in: ");
+
+let french = "Français";
+let turkish = "Türkçe";
+let arabic = "عربي";
+let russian = "русский";
+let ukrainian = "українська";
+
+export let contact = new Dictionary();
+contact.setText(english, "Contact:");
+contact.setText(german, "Kontact:")
+
+export let title = new Dictionary();
+
+title.setText(english, "Main Page");
+title.setText(german, "Hauptpage");
+
+export let intro = new Dictionary();
+intro.setText(english, "Welcome");
+intro.setText(german, "Willkommen");
+export let introText = new Dictionary();
+introText.setText(english, "Navigate Around");
+introText.setText(german, "Sehen Pages");
+
+export let legal_title = new Dictionary();
+legal_title.setText(english, "Legal Page");
+legal_title.setText(german, "Rechtliche Seite")
+export let legal_body = new Dictionary();
+legal_body.setText(english, "Click to navigate to our legal help page, it contains information like a step by step guide to the asylum process\
+                            as well as legal tools to help throughout the process.");
+legal_body.setText(german, "Klicken Sie hier, um zu unserer Rechtshilfeseite zu gelangen. Sie enthält Informationen wie einen schrittweisen Leitfaden\
+                        für das Asylverfahren und rechtliche Hilfsmittel, die während des gesamten Verfahrens helfen.");
+
+export let social_title = new Dictionary();
+social_title.setText(english, "Social Page");
+social_title.setText(german, "Soziale Seite");
+export let social_body = new Dictionary();
+social_body.setText(english, "Click to navigate to our social page, it contains places to find social services like housing, jobs, education and\
+                            healthcare.");
+social_body.setText(german, "Klicken Sie hier, um zu unserer sozialen Seite zu gelangen. Dort finden Sie Informationen zu sozialen Diensten wie\
+                            Wohnen, Arbeit, Bildung und Gesundheit.");
+
+export let community_title = new Dictionary();
+community_title.setText(english, "Commuity Page");
+community_title.setText(german, "Gemeinschaft Seite");
+export let community_body = new Dictionary();
+community_body.setText(english, "Click to navigate to our community page, it contains information like ways to connect to the local community and\
+                            meet people of all ages. It includes activities like soccer and cricket clubs.");
+community_body.setText(german, "Klicken Sie hier, um zu unserer sozialen Seite zu gelangen. Sie enthält Informationen über Möglichkeiten, sich\
+                            mit der örtlichen Gemeinschaft zu verbinden und Menschen jeden Alters zu treffen. Dazu gehören Aktivitäten wie\
+                            Fußball- und Cricket-Clubs.");
+
+export let ngo_title = new Dictionary();
+ngo_title.setText(english, "NGO Page");
+ngo_title.setText(german, "NRO Seite");
+export let ngo_body = new Dictionary();
+ngo_body.setText(english, "Click to navigate to our NGO page, it contains a sorted list of NGOS and what they can provide to asylum seekers.\
+                        The various NGOs cover various subjects like legal help, social services, and community activities.");
+ngo_body.setText(german, "Klicken Sie hier, um zu unserer NRO-Seite zu gelangen. Sie enthält eine sortierte Liste von NROs und deren Angebote\
+                        für Asylbewerber. Die verschiedenen Nichtregierungsorganisationen decken verschiedene Themenbereiche ab, wie z. B.\
+                        Rechtshilfe, soziale Dienste und Gemeinschaftsaktivitäten.");
+
+export let edu_title = new Dictionary();
+edu_title.setText(english, "Education Page");
+edu_title.setText(german, "Bildung Seite");
+export let edu_body = new Dictionary();
+edu_body.setText(english, "Click to navigate to our education page, it contains information like locations of universities, and integration\
+                            and language courses.");
+edu_body.setText(german, "Klicken Sie hier, um zu unserer Bildungsseite zu gelangen. Sie enthält Informationen wie Standorte von Universitäten,\
+                            Integrations- und Sprachkurse.");
+
+export let jobs_title = new Dictionary();
+jobs_title.setText(english, "Jobs Page");
+jobs_title.setText(german, "Jobs Seite");
+export let jobs_body = new Dictionary();
+jobs_body.setText(english, "Click to navigate to our jobs page, it contains information like locations how to create a CV and Lebenslauf,\
+                            and how to apply to jobs. It also contains links to popular job sites to get a start.");
+jobs_body.setText(german, "Klicken Sie hier, um zu unserer Jobseite zu gelangen. Sie enthält Informationen darüber, wie Sie einen Lebenslauf\
+                            erstellen und sich auf Stellen bewerben können. Sie enthält auch Links zu beliebten Stellenbörsen, um einen Anfang\
+                            zu machen.");
