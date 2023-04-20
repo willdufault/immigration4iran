18c7b4ee63fe2cf565dc10bcf83df926f0023e34
wduf <wduf02@gmail.com>
2023-04-20 11:35:17 +0200

    website layout changes and automated js to css vars

--- src/routes/pageWrapper.svelte
+++ src/routes/pageWrapper.svelte
@@ -11,25 +11,10 @@
 		@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap');
 		</style>
 </svelte:head>
-<!-- assign JS variables to CSS variables -->
-<!-- to use these variables in styling, use var(--[variable name]) -->
-<!-- ex: if you wanted to assign something to be the color stored in color1, you would do:
-	
-		color: var(--color1);
-
--->
-<main dir={Dictionary.currentLanguage === Dictionary.languages.Persian ? "rtl" : "ltr"} style="
-	--color1: {color1}; 
-	--color2: {color2}; 
-	--color3: {color3}; 
-	--color4: {color4}; 
-	--font1: {font1};
-	--font2: {font2};
-	--font3: {font3};
-	--font4: {font4};
-	"
-	>
+<main dir={Dictionary.currentLanguage === Dictionary.languages.Persian ? "rtl" : "ltr"}
+style={convertJSToCSS()}>
     <slot></slot>
+	<button on:click={() => convertJSToCSS()}>test</button>
 </main>
 <style>
 	*
@@ -51,12 +36,24 @@
 <script>
 	import { Dictionary } from "./dictionary";
 	// import JS variables
-	import { color1, color2, color3, color4, font1, font2, font3, font4} from "../theme";
+	import * as theme_vars from "../theme";
 	let langs = Dictionary.languages;
 	if(typeof localStorage !== 'undefined'){
 		Dictionary.currentLanguage = localStorage.getItem("language");
 	}
 	else{
 		Dictionary.currentLanguage = langs.English;
+	}
+
+	
+	function convertJSToCSS()
+	{
+		let res = ""; // css style string
+		let theme_keys = Object.keys(theme_vars);  // var names in theme.js
+		for(let key of theme_keys)
+		{
+			res += `--${key}: ${theme_vars[key]}; `;
+		}
+		return res;
 	}
 </script>
