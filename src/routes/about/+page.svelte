cd7c176d7b6181afdfa29e056d9aaf60765ef542
wduf <wduf02@gmail.com>
2023-04-21 14:13:42 +0200

    terms page added, about skeleton

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,14 @@
 <main>
 	<PageWrapper>
 		<Header/>
-		about us
+		<div class="title-wrapper">
+			<p>About Us</p>
+		</div>
+		<div class="body-wrapper">
+			<p>
+				about us section
+			</p>
+		</div>
 	</PageWrapper>
 </main>
 <style>
@@ -10,6 +17,25 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+	}
+
+	.title-wrapper
+	{
+		font-size: 3rem;
+		font-weight: 700;
+		display: flex;
+		justify-content: center;
+		margin: 4%;
+	}
+
+	.body-wrapper
+	{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		margin: 0 10%;
+		font-size: 1.5rem;
 	}
 </style>
 <script>
--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,7 +1,17 @@
 <main>
 	<PageWrapper>
 		<Header/>
-		terms and conditions
+		<div class="title-wrapper">
+			<p>Terms and Conditions</p>
+		</div>
+		<div class="body-wrapper">
+			<p>This website was created by students from Worcester Polytechnic Institute. Immigration4Iran
+				does not collect any personalized data from its users. The information listed on Immigration4Iran
+				can not be guaranteed in its accuracy and should not be taken for legal advice. Official
+				information can be found on <a href="https://www.bamf.de" target="_blank">bamf.de</a>. Last updated
+				April 2023. 
+			</p>
+		</div>
 	</PageWrapper>
 </main>
 <style>
@@ -10,6 +20,25 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+	}
+
+	.title-wrapper
+	{
+		font-size: 3rem;
+		font-weight: 700;
+		display: flex;
+		justify-content: center;
+		margin: 4%;
+	}
+
+	.body-wrapper
+	{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		margin: 0 10%;
+		font-size: 1.5rem;
 	}
 </style>
 <script>
